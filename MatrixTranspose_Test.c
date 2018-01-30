108ed7d7e8743ba5d394990cb9631a58d290ba19
RayzzzzzZ <393723821@qq.com>
2018-01-30 17:57:44 +0800

    Update MatrixTranspose_Test.c

--- MatrixTranspose_Test.c
+++ MatrixTranspose_Test.c
@@ -9,14 +9,15 @@ uint8_t TransList[BUFF_SIZE];
 
 typedef struct
 {
-	void (*MatrixTranspose_BackWard);
-	void (*MatrixTranspose_Foward);
-	void (*MatrixTranspose_180);
-	void (*MatrixTranspose_yFoward);
+	void (*BackWard);
+	void (*Foward);
+	void (*_180);
+	void (*xMirror);
+	void (*yMirror);
 }Matrix_Method_t;
 
 
-
+//----------------------------------------------------------------------------//
 //逆时针 
 void MatrixTranspose_BackWard(uint8_t *src, uint8_t *dst)
 {
@@ -38,12 +39,36 @@ void MatrixTranspose_Foward(uint8_t *src, uint8_t *dst)
 	}
 } 
 //180度
-void MatrixTranspose_180(uint8_t *src, uint8_t *dst);
-//X轴镜像
-void MatrixTranspose_xMirror(uint8_t *src, uint8_t *dst);
-//y轴镜像 
-void MatrixTranspose_yMirror(uint8_t *src, uint8_t *dst);
+void MatrixTranspose_180(uint8_t *src, uint8_t *dst)
+{
+	uint8_t row,column;
+	
+	for(row = 0; row < SIDE_LEN; row ++)
+	for(column = 0; column < SIDE_LEN; column ++){
+		*(dst+column+row*SIDE_LEN) = *(src+SIDE_LEN*SIDE_LEN-1-row*SIDE_LEN+column);
+	}	
+}
 
+//X轴镜像
+void MatrixTranspose_xMirror(uint8_t *src, uint8_t *dst)
+{
+	uint8_t row,column;
+	for(row = 0; row < SIDE_LEN; row ++)
+	for(column = 0; column < SIDE_LEN; column ++){
+		*(dst+column+row*SIDE_LEN) = *(src+(SIDE_LEN-row-1)*SIDE_LEN+column);
+	}		
+}
+
+//y轴镜像 
+void MatrixTranspose_yMirror(uint8_t *src, uint8_t *dst)
+{
+	uint8_t row,column;
+	for(row = 0; row < SIDE_LEN; row ++)
+	for(column = 0; column < SIDE_LEN; column ++){
+		*(dst+column+row*SIDE_LEN) = *(src+(SIDE_LEN-column-1)+SIDE_LEN*row);
+	}
+}
+//----------------------------------------------------------------------------//
 
 void Dis_Matrix(uint8_t *src)
 {
@@ -74,8 +99,12 @@ void Transpose_Test(void)
 	printf("------------------Before----------------\r\n");
 	Dis_Matrix(DataList);	
 	printf("------------------After-----------------\r\n");
-	//MatrixTranspose(DataList, TransList);
-	MatrixTranspose_Foward(DataList, TransList);
+	//MatrixTranspose_BackWard(DataList, TransList);
+	//MatrixTranspose_Foward(DataList, TransList);
+	//MatrixTranspose_180(DataList, TransList);
+	//MatrixTranspose_xMirror(DataList, TransList);
+	//MatrixTranspose_yMirror(DataList, TransList);
+	
 	Dis_Matrix(TransList);
 }
 
